c9369593c51a689eeea3b9444b2cc4609119bdaa
Alexandr Kochetkov <kochetkov-aa@keysystems.ru>
2021-07-01 13:41:43 +0300

    Добавлен условный импорт настроект для базы данных.

---
+++ config/dev.sample.py
@@ -0,0 +1,7 @@
+postgres_database_settings = {
+    'POSTGRES_SERVER': 'localhost',
+    'POSTGRES_USER': 'postgres',
+    'POSTGRES_PASSWORD': 'postgres',
+    'POSTGRES_DB': 'ksb-messworks-dev',
+    'POSTGRES_PORT': 5432
+}
